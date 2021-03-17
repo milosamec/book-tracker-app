a54f998231659cbc1ee9c682fc82500b25c64edc
Milo <milo@Milos-MacBook-Air.local>
2021-03-17 09:28:30 -0400

    added welcome screen

--- screens/AppSwitchNavigator/WelcomeScreen.js
+++ screens/AppSwitchNavigator/WelcomeScreen.js
@@ -3,10 +3,11 @@ import {View, Text} from 'react-native'
 import {Ionicons} from '@expo/vector-icons'
 import colors from '../../assets/colors'
 import CustomActionButton from '../../components/CustomActionButton'
+
 export default class WelcomeScreen extends React.Component {
     render() {
         return (
-            <View style={{flex: 1, backgroundColor: colors.bgMain}}>
+            <View style={{flex: 1, backgroundColor: colors.bgMain, paddingTop: 150}}>
                 <View style={{flex: 1, alignItems: 'center', alignContent: 'center'}}>
                     <Ionicons name="ios-bookmarks" size={150} color={colors.logoColor} />
                     <Text style={{fontSize: 50, fontWeight: '100'}}>Book Tracker</Text>
